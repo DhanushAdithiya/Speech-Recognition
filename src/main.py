59a7c30a93e14b7eca7363cdc9117c65019593d8
DhanushAdithiya <k.b.dhanush02@gmail.com>
2023-11-01 22:36:50 +0530

    Added Threading

--- src/main.py
+++ src/main.py
@@ -9,68 +9,87 @@ import sys
 import asyncio
 from dotenv import load_dotenv
 import discord
+import time
+import threading
+import signal
 
 from HiddenMarkovModelTrainer import HiddenMarkovModelTrainer
 from generateModel import generate_model
 from prediction import predict
+from monitor_audio import monitor_audio
 
 
-if not os.listdir("../model"):
-    hmm_models = generate_model("../Training2")
-else:
+def main():
     hmm_models = []
-    for file in os.listdir("../model"):
-        filename = file[:-4]
-        model = pickle.load(open(f"../model/{file}", "rb"))
-        hmm_models.append((model, filename))
+    def watch(path_to_watch):
+        print("Starting to watch")
+        if not os.listdir(path_to_watch):
+            time.sleep(1)
+        for filename in os.listdir(path_to_watch):
+            print("PRED:", predict(f"{path_to_watch}/{filename}", hmm_models))
+            os.remove(f"{path_to_watch}/{filename}")
 
-
-load_dotenv()
-token = os.getenv("TOKEN")
-user_id = os.getenv("USER_ID")
-
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents = intents)
-
-
-@client.event  
-async def on_ready():
-    while True:
-        prediction = predict("../test/db.mp3", hmm_models)
-        print(prediction)
-        match prediction:
-            case "Doorbell":
-                await client.get_channel(1099337838237061185).send("You have a visitor")
-            case "Cooker":
-                await client.get_channel(1099337838237061185).send("The cooker has blown a whistle")
-            case _: 
-                pass
-        await asyncio.sleep(10)
-
-
-client.run(token)
-
-
-
-# Options
-# 1 - Record every 2 seconds and analyze that - DRAWBACK - WILL LEAD TO MORE OVERHEAD + POSSIBLE DATA LOSS + SLOW
-# 2 - Stream the audio continously and have the model monitor it continiously - IDK HOW?
-
-# class Application():
-#     def __init__(self) -> None:
-#         self.p = pyaudio.PyAudio()
-#         self.stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True)
     
-#     async def on_ready(self):
-#         self.task = asyncio.create_task(self.listen())
+    if not os.listdir("../model"):
+        hmm_models = generate_model("../Training2")
+    else:
+        for file in os.listdir("../model"):
+            filename = file[:-4]
+            model = pickle.load(open(f"../model/{file}", "rb"))
+            hmm_models.append((model, filename))
 
-#     async def listen(self):
-#         while True:
-#             data = self.stream.read(1024)
-            
-#             if predict()
+
+    event = threading.Event()
+    t1 = threading.Thread(target=watch, args=("../temp_audios",))
+    t2 = threading.Thread(target=monitor_audio, args=(event,))
+
+    t2.daemon = True
+    t1.daemon = True
+
+    t1.start()
+    t2.start()
+
+    try:
+        while 1: 
+            time.sleep(.1)
+    except KeyboardInterrupt:
+        print("Attempting to close threads")
+        t1.join()
+        event.set()
+        t2.join()
+        print("Closed Threads Sucessfully")
+
+if __name__ == "__main__":
+    main()
+
+
+# load_dotenv()
+# token = os.getenv("TOKEN")
+# user_id = os.getenv("USER_ID")
+
+
+# intents = discord.Intents.default()
+# intents.message_content = True
+
+# client = discord.Client(intents = intents)
+
+
+# @client.event  
+# async def on_ready():
+#     while True:
+#         prediction = predict("../test/db.mp3", hmm_models)
+#         print(prediction)
+#         match prediction:
+#             case "Doorbell":
+#                 await client.get_channel(1099337838237061185).send("You have a visitor")
+#             case "Cooker":
+#                 await client.get_channel(1099337838237061185).send("The cooker has blown a whistle")
+#             case _: 
+#                 pass
+#         await asyncio.sleep(10)
+
+
+# client.run(token)
+
 
 
--- src/monitor_audio.py
+++ src/monitor_audio.py
@@ -3,44 +3,46 @@ import pyaudio
 import numpy as np
 import wave
 import audioop
-
-threshold = 50
-n_recordings = 0
+import threading
 
 
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, rate=44100, channels=1, input=True, output=False)
 
-recording = False
-recorded_audio = []
+def monitor_audio(event):
+    print("STARTING TO MONITOR AUDIO")
+    recorded_audio = []
+    threshold = 50
+    n_recordings = 0
+    recording = False
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16, rate=44100, channels=1, input=True, output=False)
+    while True:
+        data = stream.read(1024)
+        audio_array = np.frombuffer(data, dtype=np.int16)
 
-while True:
-    data = stream.read(1024)
-    audio_array = np.frombuffer(data, dtype=np.int16)
+        rms = audioop.rms(data, 2)
+        # print(rms)  
+        if rms > threshold:
+            if not recording:
+                recorded_audio = audio_array.copy()
+                recording = True
+            else:
+                recorded_audio = np.append(recorded_audio, audio_array)
 
-    rms = audioop.rms(data, 2)
-    # print(rms)  
-    if rms > threshold:
-        if not recording:
-            recorded_audio = audio_array.copy()
-            recording = True
-        else:
-            recorded_audio = np.append(recorded_audio, audio_array)
+        elif recording:
+            recording = False
 
-    elif recording:
-        recording = False
+            if len(recorded_audio) > 22050:  # Check for at least 1 second of recorded audio
+                wave_file = wave.open(f"../temp_audios/temp_audio{n_recordings}.wav", "wb")
+                wave_file.setnchannels(1)
+                wave_file.setsampwidth(2)
+                wave_file.setframerate(44100)
+                wave_file.writeframes(recorded_audio.tobytes())
+                wave_file.close()
+                n_recordings += 1
+                print("Audio recorded")
+        if event.is_set():
+            break
 
-        if len(recorded_audio) > 22050:  # Check for at least 1 second of recorded audio
-            wave_file = wave.open(f"../temp_audios/temp_audio{n_recordings}.wav", "wb")
-            wave_file.setnchannels(1)
-            wave_file.setsampwidth(2)
-            wave_file.setframerate(44100)
-            wave_file.writeframes(recorded_audio.tobytes())
-            wave_file.close()
-            n_recordings += 1
-            print("Audio recorded")
-
-            
-
-stream.stop_stream()
-stream.close()
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
